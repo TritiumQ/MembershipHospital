e1cbf5cf9b5b145108ec2bea2b072fa94a9426cf
TtrtiumQ <qun02112@126.com>
2024-06-13 09:52:38 +0800

    init

---
+++ MembershipHospital.Docter.Core/src/test/java/me/qunqun/membershiphospital/docter/core/ApplicationTests.java
@@ -0,0 +1,15 @@
+package me.qunqun.membershiphospital.docter.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests
+{
+	
+	@Test
+	void contextLoads()
+	{
+	}
+	
+}
