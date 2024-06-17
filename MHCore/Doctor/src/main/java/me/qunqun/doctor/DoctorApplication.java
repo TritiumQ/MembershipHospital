921effbea33bbb8d28415cb98ebafed82c9be239
TtrtiumQ <qun02112@126.com>
2024-06-17 09:16:24 +0800

    update

--- MHCore/Doctor/src/main/java/me/qunqun/doctor/DoctorApplication.java
+++ MHCore/Doctor/src/main/java/me/qunqun/doctor/DoctorApplication.java
@@ -1,15 +1,19 @@
 package me.qunqun.doctor;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class DoctorApplication
 {
 	
 	public static void main(String[] args)
 	{
-		SpringApplication.run(DoctorApplication.class, args);
+		var app = SpringApplication.run(DoctorApplication.class, args);
+		var host = "http://localhost:" + app.getEnvironment().getProperty("server.port");
+		log.info("\n----------------------------------------------------------\nAPI文档地址:  {}{}\nOpen API 文档:  {}{}\n----------------------------------------------------------\n", host, app.getEnvironment().getProperty("springdoc.swagger-ui.path"), host, app.getEnvironment().getProperty("springdoc.api-docs.path"));
 	}
 	
 }
